ffffcbca555fd331a383ac9a0ba7ffb1ff818744
thevipvaishnav <157284454+thevipvaishnav@users.noreply.github.com>
2024-07-22 13:30:37 +0530

    Update basic.R

--- R_Programming/Basic_Programs/basic.R
+++ R_Programming/Basic_Programs/basic.R
@@ -1,1 +1,7 @@
-
+#R program to take input from the user (name and age) and display the values. Also print the version of R installation.
+{
+NAME=readline(prompt="Enter Your Name:")
+AGE=readline(prompt="Enter Your Age:")
+print(paste("My Name is ",NAME,"And Iam ",AGE,"years old,"))
+print(R.version.string)
+}
