a68a8ac738bb29006f79155841efd808900dac67
thevipvaishnav <157284454+thevipvaishnav@users.noreply.github.com>
2024-07-22 13:36:37 +0530

    Add files via upload

---
+++ R_Programming/Basic_Programs/22_app.R
@@ -0,0 +1,29 @@
+#exercise1 print text
+"Hello World!"
+#exercise2 print number
+5
+12
+2
+#exercise3 add two numbers
+5+10
+#exercise4 print text using print function
+print("Ganesha")
+print("Sai ")
+#exercise3 comments
+#this is a comment
+#exercise4 creating and a value to a variable
+name <- "Ganesha"
+age<- 18
+name 
+age
+#exercise5  
+text <- "Ganesha"
+paste("Jai Shree",text,"ya Namaha")
+text2 <- "Anjani Puthra Aajane"
+paste("Jai Shree",text2,"ya Namaha")
+text3 <- "Sai Baba"
+paste("Jai Shree",text3,"ya Namaha")
+paste(text,text2,text3)
+num<-100
+mun<-200
+print(num+mun)
