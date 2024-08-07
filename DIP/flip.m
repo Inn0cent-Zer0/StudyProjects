2477ef5cb82cb55f1f951fb54522f5d10a996043
Inn0cent_Zer0 <157284454+Inn0cent-Zer0@users.noreply.github.com>
2024-08-07 14:00:10 +0530

    Update flip.m

--- DIP/flip.m
+++ DIP/flip.m
@@ -1,8 +1,16 @@
-origi = imread('D:/Vaishnavi/008_Matlab/cameraman.tif');
-subplot(1,2,1);
+% Read the original grayscale image from the specified file
+origi = imread('cameraman.tif');
+
+% Create a subplot for displaying multiple images in a single figure
+% Display the original image in the first subplot
+subplot(1, 2, 1);
 imshow(origi);
 title('Original Image');
+
+% Flip the original image horizontally (left to right)
 flip = fliplr(origi);
-subplot(1,2,2);
+
+% Display the flipped image in the second subplot
+subplot(1, 2, 2);
 imshow(flip);
 title('Flipped Image');
