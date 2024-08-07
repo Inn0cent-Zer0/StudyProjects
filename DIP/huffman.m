e89c4fc2a9d7f85cec9a6440e3441397a4130f2b
Inn0cent_Zer0 <157284454+Inn0cent-Zer0@users.noreply.github.com>
2024-08-07 13:58:25 +0530

    Update huffman.m

--- DIP/huffman.m
+++ DIP/huffman.m
@@ -1,3 +1,11 @@
-freqTable = =[0.2,0.4,0.1,0.1,0.2];
+% Define a frequency table for the symbols.
+% Each value represents the frequency (or probability) of the corresponding symbol.
+freqTable = [0.2, 0.4, 0.1, 0.1, 0.2];
+
+% Generate a Huffman tree based on the frequency table.
+% The huffmantree function creates a binary tree used for Huffman encoding.
 huffmanTree = huffmantree(freqTable);
-huffmanCodes = huffmanenco([1,2,3,4,5],huffmanTree);
+
+% Encode the sequence [1, 2, 3, 4, 5] using the generated Huffman tree.
+% The huffmanenco function takes the sequence and the Huffman tree to produce the encoded bit sequence.
+huffmanCodes = huffmanenco([1, 2, 3, 4, 5], huffmanTree);
